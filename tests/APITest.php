f2aca2d313c33e436ae81f5473f5c837f3d35394
Gustavo Carreno <guscarreno@gmail.com>
2024-06-11 00:08:57 +0100

    refactor: Adding `setUp`,`tearDown`

--- tests/APITest.php
+++ tests/APITest.php
@@ -13,14 +13,42 @@ const BASEURL = "http://localhost:9888";
 
 class APITest extends TestCase {
     /**
+     * API Client
+     * 
+     * @property API $api
+     */
+    private API $api;
+
+    /**
+     * setUp method
+     * 
+     * @return void
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        $client = new APIClientGuzzle(BASEURL, 5);
+        $this->api = new API($client);
+    }
+
+    /**
+     * tearDown method
+     * 
+     * @return void
+     */
+    protected function tearDown(): void {
+        unset($this->api);
+
+        parent::tearDown();
+    }
+
+    /**
      * Test the Net Info endpoint
      * 
      * @return void
      */
     public function testNetInfo(): void {
-        $client = new APIClientGuzzle(BASEURL, 5);
-        $api = new API($client);
-        $netInfo = $api->getNetInfo();
+        $netInfo = $this->api->getNetInfo();
         $this->assertIsObject($netInfo);
     }
 
